ab25a4fa1fd875af8a6d0d1a4933996a8cec53e0
grisalenajm <grisalenajm@hotmail.com>
2026-03-06 16:06:20 +0000

    fix: SesService registrado en BookingsModule, rebuild imagen

--- apps/api/src/bookings/bookings.module.ts
+++ apps/api/src/bookings/bookings.module.ts
@@ -1,10 +1,11 @@
 import { Module } from '@nestjs/common';
 import { BookingsService } from './bookings.service';
 import { BookingsController } from './bookings.controller';
+import { SesService } from './ses.service';
 
 @Module({
   controllers: [BookingsController],
-  providers: [BookingsService],
-  exports: [BookingsService],
+  providers: [BookingsService, SesService],
+  exports: [BookingsService, SesService],
 })
 export class BookingsModule {}
